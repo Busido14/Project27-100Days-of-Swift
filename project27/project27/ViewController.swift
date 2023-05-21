f292b9f4de8398849e694281ba97c31da8f332d3
Artem <artchzhen@gmail.com>
2023-05-21 19:43:17 +0900

    recoding button frame

--- project27/project27/ViewController.swift
+++ project27/project27/ViewController.swift
@@ -28,6 +28,9 @@ class ViewController: UIViewController {
         switch currentDrawType {
         case 0:
             drawRectangle()
+        case 1:
+            drawCircle()
+
             
         default:
             break
@@ -45,6 +48,24 @@ class ViewController: UIViewController {
             ctx.cgContext.setLineWidth(10)
             
             ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageVIew.image = image
+        
+    }
+    
+    func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addEllipse(in: rectangle)
             ctx.cgContext.drawPath(using: .fillStroke)
         }
         
